9c5d7443dab33012abd9a86fee348c9ee3bb0827
artemave <artemave@gmail.com>
2014-10-07 11:28:57 +0100

    don't push nodes into an array before changing them
    reminder to self: don't just copy/paste code from the internet

--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,12 @@
-var n, nodes=[],
-walk=document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
+var nodes=[],
+walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
     acceptNode: function(node) {
       return node.nodeValue.match(/\bsocial\b/i) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
     }
   }, false);
 
-while(n=walk.nextNode()) nodes.push(n);
-
-nodes.forEach(function(node) {
-    node.nodeValue = node.nodeValue.replace(/\b([sS])ocial\b/g, function($match, $1) {
-        return $1 == 's' ? 'arse' : 'Arse';
-    });
-});
+while(node = walk.nextNode()) {
+  node.nodeValue = node.nodeValue.replace(/\b([sS])ocial\b/g, function($match, $1) {
+      return $1 == 's' ? 'arse' : 'Arse';
+  });
+}
