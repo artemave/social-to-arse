1c21cbbf1b8a6804228dc70d3cb34d605ff41876
artemave <artemave@gmail.com>
2014-12-28 15:45:03 +0000

    skip inputs and editable divs

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,10 @@
 var walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
     acceptNode: function(node) {
+      //skip inputs and editable divs
+      if (node.parentNode.nodeName.match(/INPUT|TEXTAREA/) || node.parentNode.isContentEditable) {
+        return NodeFilter.FILTER_SKIP;
+      }
+
       return node.nodeValue.match(/social/i) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
     }
   }, false);
