a596b1ee9bf8b3d644ce148bf013fea3cd7d520b
Josh Chisholm <joshuachisholm@gmail.com>
2017-02-02 15:08:43 +0000

    Cors support

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var replacements = fs.readFileSync('./replacements.json')
 var port = process.env.PORT || 1337
 
 http.createServer(function (request, response) {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
   response.end(replacements, 'utf-8');
 }).listen(port)
 
